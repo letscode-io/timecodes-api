c349665aef0f15c9084e9da2fe98fff6e956d0fa
Emil Shakirov <me+github@emil.sh>
2020-05-06 19:49:23 +0200

    Extract fetching google account info to a separate file

--- cmd/auth_middleware.go
+++ cmd/auth_middleware.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"regexp"
 )
 
-const userInfoHost = "https://www.googleapis.com/oauth2/v2/userinfo"
-
 var authTokenRegExp = regexp.MustCompile(`Bearer (\S+$)`)
 
-type UserInfo struct {
-	Id      string `json:"id"`
-	Picture string `json:"picture"`
-	Email   string `json:"email"`
-}
 type CurrentUserKey struct{}
 
 func authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := getAuthToken(r.Header.Get("Authorization"))
 
-		userInfo, err := getUserInfo(token)
+		userInfo, err := fetchUserInfo(token)
 		if err != nil {
 			http.Error(w, http.StatusText(401), 401)
 			return
@@ -33,6 +22,7 @@ func authMiddleware(next http.Handler) http.Handler {
 
 		ctx := context.WithValue(r.Context(), CurrentUserKey{}, findOrCreateUser(userInfo))
 		r = r.WithContext(ctx)
+
 		next.ServeHTTP(w, r)
 	})
 }
@@ -47,35 +37,6 @@ func getAuthToken(authorizationHeader string) string {
 	token := matches[len(matches)-1]
 
 	return string(token)
-}
-
-func getUserInfo(accessToken string) (userInfo *UserInfo, err error) {
-	if len(accessToken) == 0 {
-		return nil, errors.New("accessToken cannot be empty")
-	}
-
-	url := fmt.Sprintf("%s?access_token=%s", userInfoHost, accessToken)
-	response, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
-	}
-
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
-	}
-
-	switch response.StatusCode {
-	case 401:
-		userInfo, err = nil, errors.New(string(contents))
-	default:
-		userInfo = &UserInfo{}
-		err = nil
-		json.Unmarshal(contents, userInfo)
-	}
-
-	return userInfo, err
 }
 
 func findOrCreateUser(userInfo *UserInfo) *User {
---
+++ cmd/google_api.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const userInfoHost = "https://www.googleapis.com/oauth2/v2/userinfo"
+
+type UserInfo struct {
+	Id      string `json:"id"`
+	Picture string `json:"picture"`
+	Email   string `json:"email"`
+}
+
+func fetchUserInfo(accessToken string) (userInfo *UserInfo, err error) {
+	if len(accessToken) == 0 {
+		return nil, errors.New("accessToken cannot be empty")
+	}
+
+	url := fmt.Sprintf("%s?access_token=%s", userInfoHost, accessToken)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	switch response.StatusCode {
+	case 401:
+		userInfo, err = nil, errors.New(string(contents))
+	default:
+		userInfo = &UserInfo{}
+		err = nil
+		json.Unmarshal(contents, userInfo)
+	}
+
+	return userInfo, err
+}
