78979d18e4306ef6a556eeaeae65fb44f8f18544
Emil Shakirov <me+github@emil.sh>
2020-05-10 12:14:01 +0200

    Create TimecodeRepository

--- cmd/timecode_repository.go
+++ cmd/timecode_repository.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	timecodeParser "timecodes/cmd/timecode_parser"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,4 +13,57 @@ type Timecode struct {
 	Seconds     int            `json:"seconds" gorm:"not null"`
 	VideoID     string         `json:"videoId" gorm:"not null;index"`
 	Likes       []TimecodeLike `json:"likes" gorm:"foreignkey:TimecodeID"`
+}
+
+type TimecodeRepository interface {
+	FindByVideoId(string) (*[]*Timecode, error)
+	Create(*Timecode) (*Timecode, error)
+	CreateFromParsedCodes([]timecodeParser.ParsedTimeCode, string) (*[]*Timecode, error)
+}
+
+type DBTimecodeRepository struct {
+	DB *gorm.DB
+}
+
+func (repo *DBTimecodeRepository) FindByVideoId(videoID string) (*[]*Timecode, error) {
+	timecodes := &[]*Timecode{}
+
+	err := repo.DB.Order("seconds asc").
+		Preload("Likes").
+		Where(&Timecode{VideoID: videoID}).
+		Find(timecodes).
+		Error
+
+	return timecodes, err
+}
+
+func (repo *DBTimecodeRepository) Create(timecode *Timecode) (*Timecode, error) {
+	err := repo.DB.Create(timecode).Error
+
+	return timecode, err
+}
+
+func (repo *DBTimecodeRepository) CreateFromParsedCodes(parsedTimecodes []timecodeParser.ParsedTimeCode, videoId string) (*[]*Timecode, error) {
+	seen := make(map[string]struct{})
+
+	var collection []*Timecode
+	var err error
+	for _, code := range parsedTimecodes {
+		key := string(code.Seconds) + code.Description
+		if _, ok := seen[key]; ok {
+			continue
+		}
+
+		seen[key] = struct{}{}
+
+		timecode := &Timecode{Seconds: code.Seconds, VideoID: videoId, Description: code.Description}
+
+		err = repo.DB.Create(timecode).Error
+		if err != nil {
+			continue
+		}
+		collection = append(collection, timecode)
+	}
+
+	return &collection, err
 }
