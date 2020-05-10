8f4e4bfaef64669082e355740d549d37e1711049
Emil Shakirov <me+github@emil.sh>
2020-05-10 12:14:20 +0200

    Create TimecodeLikeRepository

--- cmd/timecode_like_repository.go
+++ cmd/timecode_like_repository.go
@@ -7,3 +7,31 @@ type TimecodeLike struct {
 	TimecodeID uint `json:"timecodeId" gorm:"not null"`
 	UserID     uint `json:"userId" gorm:"not null"`
 }
+
+type TimecodeLikeRepository interface {
+	Create(*TimecodeLike, uint) (*TimecodeLike, error)
+	Delete(*TimecodeLike, uint) (*TimecodeLike, error)
+}
+
+type DBTimecodeLikeRepository struct {
+	DB *gorm.DB
+}
+
+func (repo *DBTimecodeLikeRepository) Create(timecodeLike *TimecodeLike, userID uint) (*TimecodeLike, error) {
+	timecodeLike.UserID = userID
+
+	err := repo.DB.Create(timecodeLike).Error
+
+	return timecodeLike, err
+}
+
+func (repo *DBTimecodeLikeRepository) Delete(timecodeLike *TimecodeLike, userID uint) (*TimecodeLike, error) {
+	err := repo.DB.Where(&TimecodeLike{UserID: userID, TimecodeID: timecodeLike.TimecodeID}).First(timecodeLike).Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = repo.DB.Unscoped().Delete(timecodeLike).Error
+
+	return timecodeLike, err
+}
