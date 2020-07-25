94fc97049ee20a17b545a39def8750d6cb16ba83
Emil Shakirov <me+github@emil.sh>
2020-07-25 14:10:26 +0200

    Fix mocks usage in likes controller tests

--- cmd/timecode_likes_controller_test.go
+++ cmd/timecode_likes_controller_test.go
@@ -27,43 +27,26 @@ type MockTimecodeLikeRepository struct {
 
 func (mr *MockTimecodeLikeRepository) Create(like *TimecodeLike, userID uint) (*TimecodeLike, error) {
 	args := mr.Called(like, userID)
-	err := args.Error(1)
 
-	like.UserID = userID
-
-	if like.TimecodeID == invalidTimecodeID {
-		return nil, err
-	}
-
-	_ = args.Get(0).(*TimecodeLike)
-
-	return like, nil
+	return args.Get(0).(*TimecodeLike), args.Error(1)
 }
 
 func (mr *MockTimecodeLikeRepository) Delete(like *TimecodeLike, userID uint) (*TimecodeLike, error) {
 	args := mr.Called(like, userID)
-	err := args.Error(1)
 
-	like.UserID = userID
-
-	if like.TimecodeID == invalidTimecodeID {
-		return nil, err
-	}
-
-	_ = args.Get(0).(*TimecodeLike)
-
-	return like, nil
+	return args.Get(0).(*TimecodeLike), args.Error(1)
 }
 
 func Test_handleCreateTimecodeLike(t *testing.T) {
 	url := "/auth/timecode_likes"
+	currentUser := &User{}
+	currentUser.ID = 1
 
 	t.Run("when creation is successful", func(t *testing.T) {
-		currentUser := &User{}
-		currentUser.ID = 1
+		likeParams := &TimecodeLike{TimecodeID: 5}
+		like := &TimecodeLike{TimecodeID: 5, UserID: currentUser.ID}
 
-		like := &TimecodeLike{TimecodeID: 5}
-		mockTKRepo.On("Create", like, currentUser.ID).Return(like, nil)
+		mockTKRepo.On("Create", likeParams, currentUser.ID).Return(like, nil)
 
 		params := []byte(`{ "timecodeId": 5 }`)
 		req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(params))
@@ -75,11 +58,8 @@ func Test_handleCreateTimecodeLike(t *testing.T) {
 	})
 
 	t.Run("when creation has been failed", func(t *testing.T) {
-		currentUser := &User{}
-		currentUser.ID = 1
-
 		like := &TimecodeLike{TimecodeID: invalidTimecodeID}
-		mockTKRepo.On("Create", like, currentUser.ID).Return(nil, errors.New(""))
+		mockTKRepo.On("Create", like, currentUser.ID).Return(&TimecodeLike{}, errors.New(""))
 
 		params := []byte(fmt.Sprintf(`{ "timecodeId": %d }`, invalidTimecodeID))
 		req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(params))
@@ -103,10 +83,10 @@ func Test_handleCreateTimecodeLike(t *testing.T) {
 
 func Test_handleDeleteTimecodeLike(t *testing.T) {
 	url := "/auth/timecode_likes"
+	currentUser := &User{}
+	currentUser.ID = 1
 
 	t.Run("when deletion is successful", func(t *testing.T) {
-		currentUser := &User{}
-		currentUser.ID = 1
 
 		like := &TimecodeLike{TimecodeID: 5}
 		mockTKRepo.On("Delete", like, currentUser.ID).Return(like, nil)
@@ -121,11 +101,8 @@ func Test_handleDeleteTimecodeLike(t *testing.T) {
 	})
 
 	t.Run("when deletion has been failed", func(t *testing.T) {
-		currentUser := &User{}
-		currentUser.ID = 1
-
 		like := &TimecodeLike{TimecodeID: invalidTimecodeID}
-		mockTKRepo.On("Delete", like, currentUser.ID).Return(nil, errors.New(""))
+		mockTKRepo.On("Delete", like, currentUser.ID).Return(&TimecodeLike{}, errors.New(""))
 
 		params := []byte(fmt.Sprintf(`{ "timecodeId": %d }`, invalidTimecodeID))
 		req, _ := http.NewRequest(http.MethodDelete, url, bytes.NewBuffer(params))
